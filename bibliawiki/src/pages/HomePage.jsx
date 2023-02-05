89a1b9276df09bc2be79a5e3d7520ae04f7e7d2a
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-02-05 18:26:26 -0300

    Adiciona classNames no componente HomePage

--- bibliawiki/src/pages/HomePage.jsx
+++ bibliawiki/src/pages/HomePage.jsx
@@ -1,19 +1,22 @@
 export default function HomePage() {
   return (
-    <section>
-      <div>
+<section className="home-page">
+      <div className="container-btn-home-page">
         <button
           type="button"
+          className="btn-home-page"
         >
           Ir Para BibliaWiki
         </button>
         <button
           type="button"
+          className="btn-home-page"
         >
           Pedir Oração
         </button>
         <button
           type="button"
+          className="btn-home-page"
         >
           Tirar Dúvida sobre Bíblia
         </button>
