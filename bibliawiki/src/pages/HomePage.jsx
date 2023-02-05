6a1dc20f6b4d0693a454ba9af44028f46ca8930f
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-02-05 18:25:40 -0300

    Adiciona botões no componente HomePage

--- bibliawiki/src/pages/HomePage.jsx
+++ bibliawiki/src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
 export default function HomePage() {
   return (
-    <section >
-      
+    <section>
+      <div>
+        <button
+          type="button"
+        >
+          Ir Para BibliaWiki
+        </button>
+        <button
+          type="button"
+        >
+          Pedir Oração
+        </button>
+        <button
+          type="button"
+        >
+          Tirar Dúvida sobre Bíblia
+        </button>
+      </div>
     </section>
   )
 }
