84a86503ba906cebd8f9de732db9f0a6b2d112c5
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-02-05 18:38:13 -0300

    Ajusta o export default no App.jsx

--- bibliawiki/src/App.jsx
+++ bibliawiki/src/App.jsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
-function App() {
+export default function App() {
   return (
     <Routes>
         <Route exact path="/" element={ <HomePage /> } />
       </Routes>
   );
 }
-
-export default App;
