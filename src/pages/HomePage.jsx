eda1614fb27c107fb29c14d5a377522dfd78cac7
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-06-16 13:17:22 -0300

    adiciona terceiro link + ajustes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,12 +5,19 @@ export default function HomePage() {
   return (
     <section className="home-page">
       <div className="container-btn-home-page">
-        <button
-          type="button"
+
+        <a
+          href="http://bibliawiki.pbworks.com/w/page/147603879/BibliaWiki"
           className="btn-home-page"
+          target="_blank" rel="noreferrer"
         >
-          Ir Para BibliaWiki
-        </button>
+          <button
+            type="button"
+            className="btn-home-page"
+          >
+            Ir Para BibliaWiki
+          </button>
+        </a>
         <a
           href="https://wa.me/5547999911719"
           className="btn-home-page"
