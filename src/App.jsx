12971d03cdf995eda8c35a0ad90c93a8791b47b9
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-07-12 21:15:40 -0300

    Cria página Sobre Nós

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import SobreNos from './pages/SobreNos';
 
 export default function App() {
   return (
     <Routes>
         <Route exact path="/" element={ <HomePage /> } />
+        <Route exact path="/" element={ <SobreNos /> } />
       </Routes>
   );
 }
---
+++ src/pages/SobreNos.jsx
@@ -0,0 +1,39 @@
+import '../css/HomePage.css';
+
+export default function SobreNos() {
+  return (
+    <section className="home-page">
+      <div className="container-btn-home-page">
+        <a
+          href="http://bibliawiki.pbworks.com/w/page/147603879/BibliaWiki"
+          className="btn-home-page"
+          target="_blank" rel="noreferrer"
+        >
+            Sobre Nós
+        </a>
+
+        <a
+          href="http://bibliawiki.pbworks.com/w/page/147603879/BibliaWiki"
+          className="btn-home-page"
+          target="_blank" rel="noreferrer"
+        >
+            Ir Para BibliaWiki
+        </a>
+        <a
+          href="https://wa.me/5547999911719"
+          className="btn-home-page"
+          target="_blank" rel="noreferrer"
+        >
+          Pedir Oração
+        </a>
+        <a
+          href="https://wa.me/5547999911719"
+          className="btn-home-page"
+          target="_blank" rel="noreferrer"
+        >
+          Grupo de Estudos da Bíblia
+        </a>
+      </div>
+    </section>
+  )
+}
